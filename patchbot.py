eaa095787f458edeeddd15e3b706dcf2c6d4ed52
John Dickinson <me@not.mn>
2018-08-10 15:31:07 -0700

    calculate and display total time spent in CI

--- patchbot.py
+++ patchbot.py
@@ -1,6 +1,7 @@
 import sopel.module
 import requests
 import json
+import re
 
 REVIEW_SERVER = 'https://review.openstack.org'
 KNOWN_BOTS = set([
@@ -8,6 +9,43 @@ KNOWN_BOTS = set([
     'openstackgerrit',
     'openstackstatus',
 ])
+
+
+lr_regex = re.compile(r'^.* in (?:(\d+)h )?(?:(\d+)m )?(\d+)s$')
+
+
+def tim_time_calc(patch_number):
+
+    def hms_to_i(h, m, s):
+        return int(s or '0') + 60 * (int(m or '0') + 60 * int(h or '0'))
+
+    def i_to_hms(i):
+        m, s = divmod(i, 60)
+        h, m = divmod(m, 60)
+        return h, m, s
+
+    c = requests.get('https://review.openstack.org/changes/%s/detail' % patch_number).content
+    d = json.loads(c[4:])
+    m = [(x['date'], x['message']) for x in d['messages']
+         if x.get('author', {}).get('username') in ('zuul', 'jenkins')]
+    total_cpu = 0
+    total_real = 0
+    for d, m in m:
+        run_total = 0
+        max_for_run = 0
+        for l in m.split('\n'):
+            lm = lr_regex.match(l)
+            if not lm:
+                continue
+            t = hms_to_i(*lm.groups())
+            run_total += t
+            if t > max_for_run:
+                max_for_run = t
+        if not run_total:
+            continue
+        total_cpu += run_total
+        total_real += max_for_run
+    return i_to_hms(total_cpu), i_to_hms(total_real)
 
 #TODO: memoize this
 def _get_data(patch_number):
@@ -60,6 +98,12 @@ def get_response(patch_number, already_linked=True):
             pieces.append(subject)
         else:
             pieces.append('%s (%s)' % (subject, status))
+    cpu_time, wall_time = tim_time_calc(patch_number)
+    if any(cpu_time + wall_time):
+        # time_message = '%2dh %2dm %2ds cpu' % cpu_time
+        # pieces.append(time_message)
+        time_message = '%dh %dm %ds spent in CI' % wall_time
+        pieces.append(time_message)
     return ' - '.join(pieces)
 
 #TODO: be able to mix the forms
